9c6281313cd84bc253d235c5529cb12c9e89d781
edgtzolmedo <egutierrezolmedo@gmail.com>
2020-07-30 19:49:07 -0500

    Add reactivity

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,5 +1,6 @@
 import { LogService } from './log.service';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class StarWarsService {
@@ -8,6 +9,7 @@ export class StarWarsService {
     { name: 'Darth Vader', side: '' }
   ];
   private logService: LogService;
+  charactersChanged = new Subject<void>();
 
   constructor(logService: LogService){
     this.logService = logService;
@@ -27,6 +29,7 @@ export class StarWarsService {
       return char.name === charInfo.name;
     });
     this.characters[pos].side = charInfo.side;
+    this.charactersChanged.next();
     this.logService.writeLog('Changed side of ' + charInfo.name + ', new side: ' + charInfo.side);
   }
 
